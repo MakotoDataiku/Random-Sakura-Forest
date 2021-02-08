69f2333398fda2b2d45c78ecd06a7d08ebba5b8e
admin <makoto.miyazaki@dataiku.com>
2021-02-08 16:04:53 +0100

    Recipe CHERRYBLOSSOM.compute_blooming_days_fixed saved through notebook notebook editor for compute_blooming_days_fixed

--- recipes/compute_blooming_days_fixed.py
+++ recipes/compute_blooming_days_fixed.py
@@ -1,20 +1,43 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 blooming_days_joined = dataiku.Dataset("blooming_days_joined")
-blooming_days_joined_df = blooming_days_joined.get_dataframe()
+df = blooming_days_joined.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+d = {}
+for year in df.year.unique():
+    blooming_day = df[df.year==year].blooming_day.unique()[0]
+    d[year] = blooming_day
 
-blooming_days_fixed_df = blooming_days_joined_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+d
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_bloom = df[df.flower_status=='bloom']
+df_bloom.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df_new = pd.DataFrame()
+for year in df.year.unique():
+    df_sub = df[df.year==year]
+    df_sub['blooming_day_shift'] = df_sub["blooming_day"]
+    blooming_ind = df_bloom[df_bloom.year==year].index[0]
+    if year < 2019:
+        df_sub.loc[df_sub.index > blooming_ind, "blooming_day_shift"] = d[year+1]
+    else:
+        None
+    df_new = pd.concat([df_new, df_sub])
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 blooming_days_fixed = dataiku.Dataset("blooming_days_fixed")
-blooming_days_fixed.write_with_schema(blooming_days_fixed_df)
+blooming_days_fixed.write_with_schema(df_new)
