4bdf0f5cb0d320d61f0dff0e29aa61fa148a8e03
admin <makoto.miyazaki@dataiku.com>
2021-02-08 13:25:10 +0100

    Recipe CHERRYBLOSSOM.compute_not_blooming_yet saved through notebook notebook editor for compute_not_blooming_yet

--- recipes/compute_not_blooming_yet.py
+++ recipes/compute_not_blooming_yet.py
@@ -1,20 +1,27 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 days_till_bloom = dataiku.Dataset("days_till_bloom")
-days_till_bloom_df = days_till_bloom.get_dataframe()
+df = days_till_bloom.get_dataframe().sort_values('date', ascending=True)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+new_df = pd.DataFrame()
+for year in df.year.unique():
+    for index, row in df.iterrows():
+        if row['flower_status']!='bloom':
+            new_df = new_df.append(row, ignore_index=False)
+        else:
+            break
 
-not_blooming_yet_df = days_till_bloom_df # For this sample code, simply copy input to output
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 not_blooming_yet = dataiku.Dataset("not_blooming_yet")
-not_blooming_yet.write_with_schema(not_blooming_yet_df)
+not_blooming_yet.write_with_schema(new_df)
